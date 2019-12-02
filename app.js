ee34027211342ea7ea2bd65ecdd085bfaae4974b
sh183694@gmail.com <sh183694@gamil.com>
2019-12-02 15:20:06 +0900

    OPTIMIZE code

--- app.js
+++ app.js
@@ -8,35 +8,67 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topic", (req, res) => {
-  fs.readdir("data", (err, files) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Internal Server Error");
-    }
-    res.render("view", { topics: files });
-  });
-});
-app.get("/topic/:id", (req, res) => {
-  var id = req.params.id;
-
-  fs.readdir("data", (err, files) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Internal Server Error");
-    }
-    fs.readFile("data/" + id, "utf8", (err, data) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
-      }
-      res.render("view", { topics: files, title: id, description: data });
-    });
-  });
-});
 app.get("/topic/new", (req, res) => {
-  res.render("new");
+  fs.readdir("data", (err, files) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    }
+    res.render("new", { topics: files });
+  });
 });
+
+app.get(["/topic", "/topic/:id"], (req, res) => {
+  fs.readdir("data", (err, files) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    }
+    var id = req.params.id;
+    if (id) {
+      fs.readFile("data/" + id, "utf8", (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Internal Server Error");
+        }
+        res.render("view", { topics: files, title: id, description: data });
+      });
+    } else {
+      res.render("view", {
+        topics: files,
+        title: "Welcome",
+        description: "Hello JavaScript for server."
+      });
+    }
+  });
+});
+
+// app.get("/topic", (req, res) => {
+//   fs.readdir("data", (err, files) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send("Internal Server Error");
+//     }
+//     res.render("view", { topics: files });
+//   });
+// });
+// app.get("/topic/:id", (req, res) => {
+//   var id = req.params.id;
+
+//   fs.readdir("data", (err, files) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send("Internal Server Error");
+//     }
+//     fs.readFile("data/" + id, "utf8", (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         res.status(500).send("Internal Server Error");
+//       }
+//       res.render("view", { topics: files, title: id, description: data });
+//     });
+//   });
+// });
 
 app.post("/topic", (req, res) => {
   const title = req.body.title;
@@ -45,7 +77,7 @@ app.post("/topic", (req, res) => {
     if (err) {
       res.status(500).send("Internal Server Error");
     }
-    res.send("Success");
+    res.redirect("/topic/" + title);
   });
 });
 
